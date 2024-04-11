fe09f6565f206ae1673465e5d76b82917e88d020
EmelyTarazonaPerez <emelydayannapt@ufps.edu.co>
2024-04-11 18:43:47 -0500

    historia de usuario HU-005

---
+++ src/main/java/plazoleta/adapters/driving/http/JwtService/JwtTokenValidator.java
@@ -0,0 +1,26 @@
+package plazoleta.adapters.driving.http.JwtService;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtTokenValidator {
+    private static final String SECRET_KEY = "586E3272357538782F413F4428472B4B6250655368566B597033733676397924";
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(SECRET_KEY).build().parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public String extractRole(String token) {
+        Jws<Claims> claims = Jwts.parserBuilder().setSigningKey(SECRET_KEY).build().parseClaimsJws(token);
+
+        return claims.getBody().get("rol", String.class);
+    }
+}
---
+++ src/main/java/plazoleta/config/Auth/SecurityConfig.java
@@ -0,0 +1,35 @@
+package plazoleta.config.Auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import plazoleta.adapters.driving.http.JwtService.JwtTokenValidationFilter;
+import plazoleta.adapters.driving.http.JwtService.JwtTokenValidator;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final JwtTokenValidator tokenValidator;
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authRequest -> authRequest
+                        .requestMatchers(HttpMethod.POST, "/plate/**").hasAuthority("propietario")
+                        .requestMatchers(HttpMethod.POST, "/restaurant/**").hasAuthority("admin")
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(new JwtTokenValidationFilter(tokenValidator), UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+}
+

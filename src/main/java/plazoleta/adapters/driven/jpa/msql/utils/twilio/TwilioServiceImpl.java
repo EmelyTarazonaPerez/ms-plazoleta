e4db9b6705d588e69dde6888a69491182c3ef7bf
EmelyTarazonaPerez <emelydayannapt@ufps.edu.co>
2024-04-16 20:28:38 -0500

    changes

--- src/main/java/plazoleta/adapters/driven/jpa/msql/utils/twilio/TwilioServiceImpl.java
+++ src/main/java/plazoleta/adapters/driven/jpa/msql/utils/twilio/TwilioServiceImpl.java
@@ -5,11 +5,11 @@ import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 public class TwilioServiceImpl {
     private TwilioServiceImpl() {throw new IllegalStateException("Utility class");}
-    public static final String ACCOUNT = "";
+    public static final String ACCOUNT_SID = "";
     public static final String AUTH_TOKEN ="";
     public static final String NUMBER = "";
     public static String sendMSM (String numberClient, String pin) {
-        Twilio.init(ACCOUNT, AUTH_TOKEN);
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
         Message message = Message.creator(
                         new com.twilio.type.PhoneNumber("+57" + numberClient),
                         new com.twilio.type.PhoneNumber(NUMBER),
